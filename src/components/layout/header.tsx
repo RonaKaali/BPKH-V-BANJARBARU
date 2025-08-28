58c9fd74d491c0fb7660b6c5c0595c8f0ff495b1
KAALI'S GAMING <thekaali0@gmail.com>
2025-08-28 15:04:39 +0000

    ganti logo daun jatuh nya jadi logo nya ini

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,10 +2,9 @@
 'use client';
 
 import Link from 'next/link';
-import { Leaf } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const navLinks = [
     { href: '/', label: 'Beranda' },
@@ -23,9 +22,9 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-20 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Leaf className="h-6 w-6 text-primary" />
+          <Image src="https://firebasestorage.googleapis.com/v0/b/app-interviews-dev.appspot.com/o/user-uploads%2F1719572635956-kemenhut.png?alt=media&token=e92f3909-5a98-4c62-85a2-099496464520" alt="Logo Kementerian Kehutanan" width={50} height={50} />
           <span className="font-bold font-headline sm:inline-block">
             BPKH Wilayah V 
             Banjarbaru          </span>
