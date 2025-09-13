d36833746ab650caed0a253abf4c9eabcf9d80a3
KAALI'S GAMING <thekaali0@gmail.com>
2025-09-13 09:23:05 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,18 +7,12 @@
   # Sets environment variables in the workspace
   env = {};
 
-  # Search for the starship package in nixpkgs
-  # Adds it to the shell
-  # You can also use legacyPackages.starship
-  pre-init = ''
-    mkdir -p .local/share/mongodb
-    mongod --dbpath .local/share/mongodb &
-  '';
-
   # The main shell to start when opening the workspace
   # Sets the welcome message as well
   start = ''
     echo "Welcome to your new workspace!"
+    mkdir -p .local/share/mongodb
+    mongod --dbpath .local/share/mongodb &
     # The command to start your application
     npm run dev
   '';
